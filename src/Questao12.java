1f79f9bf5625967a376547ba3036231ed1b6eeb3
Cainan Lima <cainan.lima25@gmail.com>
2023-08-06 22:59:16 -0300

    resolve questao 12

---
+++ src/Questao12.java
@@ -0,0 +1,11 @@
+public class Questao12 {
+    public static void main(String[] args) {
+        double result;
+        for (int i = 1; i <= 100; i++) {
+            if(i%3 ==0){
+                result = i/2f;
+                System.out.println(result);
+            }
+        }
+    }
+}
