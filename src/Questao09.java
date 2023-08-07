1e3b8a190e870dd725f5f24907f163e2a3692396
Cainan Lima <cainan.lima25@gmail.com>
2023-08-06 22:42:31 -0300

    resolve questao 09

---
+++ src/Questao09.java
@@ -0,0 +1,7 @@
+public class Questao09 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("O quadrado de %d é %d!\n" , i, (int)Math.pow(i,2));
+        }
+    }
+}
